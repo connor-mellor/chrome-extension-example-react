0e0ef30e6c53b03ca6409eea3c0d74b7a8976d18
connor-mellor <Connor_Mellor@hotmail.com>
2024-09-21 01:46:22 +0100

    feat: initial commit for react+vite chrome extension

---
+++ server/src/Popup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Popup: React.FC = () => {
+  return (
+    <div>
+      <h1>Chrome Extension Popup</h1>
+      <p>This is a simple popup powered by React + Vite + TypeScript.</p>
+    </div>
+  );
+};
+
+export default Popup;
